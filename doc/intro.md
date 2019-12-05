31acd4ef612912167a9a155fd96d755d7466d70b
Armando Locay <alocay@gmail.com>
2019-12-04 21:54:49 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to genetic-image-reagent
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/genetic_image_reagent/core_test.clj
@@ -0,0 +1,7 @@
+(ns genetic-image-reagent.core-test
+  (:require [clojure.test :refer :all]
+            [genetic-image-reagent.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
